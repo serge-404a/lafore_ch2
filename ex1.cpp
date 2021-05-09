7469401b2a9015f5e1303575b566f0f68ec6626b
Serge <legrente@gmail.com>
2021-05-09 19:45:38 +0300

    setting gitignore in VS

--- ex1.cpp
+++ ex1.cpp
@@ -1,4 +1,10 @@
-//
-// Created by lEgre on 5/9/2021.
-//
-
+#include <iostream>
+using namespace std;
+int main() {
+    float ft, gallon;
+    cout << "Input number of galons\n";
+    cin >> gallon;
+    ft = gallon * 7.481;
+    cout << "Temp in Celsius is " << ft << '\n';
+    //std::cout << "Hello, World!" << std::endl;
+    return 0;
